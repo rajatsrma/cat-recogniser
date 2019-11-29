1ab23fe104b5179535e2cf0c7f0da9c4c2296372
rajatsrma <rajatsharma1203@gmail.com>
2019-11-29 17:10:42 +0530

    cat recogniser usin nn

--- Logistic+Regression+with+a+Neural+Network+mindset+v5.py
+++ Logistic+Regression+with+a+Neural+Network+mindset+v5.py
@@ -1,28 +1,4 @@
 
-# coding: utf-8
-
-# # Logistic Regression with a Neural Network mindset
-# 
-# Welcome to your first (required) programming assignment! You will build a logistic regression classifier to recognize  cats. This assignment will step you through how to do this with a Neural Network mindset, and so will also hone your intuitions about deep learning.
-# 
-# **Instructions:**
-# - Do not use loops (for/while) in your code, unless the instructions explicitly ask you to do so.
-# 
-# **You will learn to:**
-# - Build the general architecture of a learning algorithm, including:
-#     - Initializing parameters
-#     - Calculating the cost function and its gradient
-#     - Using an optimization algorithm (gradient descent) 
-# - Gather all three functions above into a main model function, in the right order.
-
-# ## 1 - Packages ##
-# 
-# First, let's run the cell below to import all the packages that you will need during this assignment. 
-# - [numpy](https://www.numpy.org/) is the fundamental package for scientific computing with Python.
-# - [h5py](http://www.h5py.org) is a common package to interact with a dataset that is stored on an H5 file.
-# - [matplotlib](http://matplotlib.org) is a famous library to plot graphs in Python.
-# - [PIL](http://www.pythonware.com/products/pil/) and [scipy](https://www.scipy.org/) are used here to test your model with your own picture at the end.
-
 # In[1]:
 
 import numpy as np
